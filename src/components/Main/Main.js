3fe5b1104f4d76c47a4f9e27e4cf42cdabc51a40
Anna Golmakova <ann.golmakova@gmail.com>
2023-11-07 09:29:20 +0500

    сделала NavTab

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import './Main.css'
 import Promo from '../Promo/Promo.js';
+import NavTab from '../NavTab/NavTab.js';
 
 function Main() {
 
     return (
         <main className="movies">
             <Promo />
+            <NavTab />
         </main>
     );
 }
