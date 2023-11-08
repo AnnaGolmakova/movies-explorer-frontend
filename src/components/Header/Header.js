edfc9d9ffb38614a70ec391bd35609b341e415fa
Anna Golmakova <ann.golmakova@gmail.com>
2023-11-08 19:14:08 +0500

    внесла правки в компоненты

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,38 +11,40 @@ function Header({ isAuthorized }) {
 
     return (
         <header className={`${location.pathname === "/" ? "header header_main" : ""} header`} >
-            <Link to="/">
-                <img src={logo} className="header__logo" alt="Лого" />
-            </Link>
-            {isAuthorized &&
-                <>
-                    <nav className="header__navigation">
-                        <ul className="header__navigation-items">
-                            <li>
-                                <Link
-                                    to="/movies"
-                                    className={`header__navigation-item ${location.pathname === "/movies" ? "header__navigation-item_active" : ""}`}
-                                >
-                                    Фильмы
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/saved-movies" className={`header__navigation-item ${location.pathname === "/saved-movies" ? "header__navigation-item_active" : ""}`}>Сохраненные фильмы</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <button type="button" className="header__account-button" onClick={() => navigate("/profile")}>
-                        Аккаунт
-                    </button>
-                    <Navigation className="menu-button" />
-                </>
-            }
-            {!isAuthorized &&
-                <div className="login-buttons">
-                    <button type="button" className="registration-button" onClick={() => navigate("/signup")}>Регистрация</button>
-                    <button type="button" className="login-button" onClick={() => navigate("/signin")}>Войти</button>
-                </div>
-            }
+            <div className="header__container">
+                <Link to="/">
+                    <img src={logo} className="header__logo" alt="Лого" />
+                </Link>
+                {isAuthorized &&
+                    <>
+                        <nav className="header__navigation">
+                            <ul className="header__navigation-items">
+                                <li>
+                                    <Link
+                                        to="/movies"
+                                        className={`header__navigation-item ${location.pathname === "/movies" ? "header__navigation-item_active" : ""}`}
+                                    >
+                                        Фильмы
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/saved-movies" className={`header__navigation-item ${location.pathname === "/saved-movies" ? "header__navigation-item_active" : ""}`}>Сохраненные фильмы</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <button type="button" className="header__account-button" onClick={() => navigate("/profile")}>
+                            Аккаунт
+                        </button>
+                        <Navigation className="menu-button" />
+                    </>
+                }
+                {!isAuthorized &&
+                    <div className="login-buttons">
+                        <button type="button" className="registration-button" onClick={() => navigate("/signup")}>Регистрация</button>
+                        <button type="button" className="login-button" onClick={() => navigate("/signin")}>Войти</button>
+                    </div>
+                }
+            </div>
         </header >
     )
 }
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,17 +2,26 @@ import './Movies.css';
 import MoviesCardList from '../MoviesCardList/MoviesCardList.js';
 import SearchForm from '../SearchForm/SearchForm.js';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox.js';
+import Preloader from '../Preloader/Preloader.js';
 
 
 function Movies({ movies }) {
+    const isLoading = true
 
     return (
         <main className="movies">
-            <section className="search-section">
-                <SearchForm />
-                <FilterCheckbox />
-            </section>
-            <MoviesCardList movies={movies}></MoviesCardList>
+            {isLoading &&
+                <Preloader />
+            }
+            {!isLoading &&
+                <>
+                    <section className="search-section">
+                        <SearchForm />
+                        <FilterCheckbox />
+                    </section>
+                    <MoviesCardList movies={movies}></MoviesCardList>
+                </>
+            }
         </main>
     );
 }
